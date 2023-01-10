e773009e7391fff480da029b266af663c6766624
Ishro Dev <dev@ishro.com>
2023-01-10 13:37:54 +0400

    BUG FIX

--- src/helpers/mt5Commands/getProductPrice.js
+++ src/helpers/mt5Commands/getProductPrice.js
@@ -1,3 +1,12 @@
+let gramPrice = 233.75;
+let kiloPrice = 233000.75;
+
 exports.getPrice = (quantity, unit) => {
-  return parseInt(quantity) * 233.75;
+  let total;
+  if (unit == "gr") {
+    total = parseInt(quantity) * gramPrice;
+  } else if (unit == "kl") {
+    total = parseInt(quantity) * kiloPrice;
+  }
+  return total;
 };
