12927eac5c5fb946171665af87b13790b1b318ea
Ishro Dev <dev@ishro.com>
2023-01-20 18:04:03 +0400

    BUG FIX

--- src/helpers/mt5Commands/getProductPrice.js
+++ src/helpers/mt5Commands/getProductPrice.js
@@ -1,18 +1,30 @@
 const axios = require("axios");
 
+function getMonth(month) {
+  month = month + 1;
+  return month < 10 ? "0" + month.toString() : month.toString();
+}
+
 exports.getGramPrice = async (quantity) => {
+  const today = new Date();
   const options = {
     headers: {
       Accept: "application/json",
       "x-access-token": "goldapi-7ygrtld4hhnh7-io",
     },
   };
+  let api_url =
+    "https://www.goldapi.io/api/XAU/USD/" +
+    today.getFullYear() +
+    getMonth(today.getMonth()) +
+    (today.getDate() - 1);
   return axios
-    .get("https://www.goldapi.io/api/XAU/USD/20230119", options)
+    .get(api_url, options)
     .then((res) => {
       return res.data.price_gram_24k * quantity;
     })
     .catch((err) => {
+      console.log(err);
       return 61 * quantity;
     });
 };
