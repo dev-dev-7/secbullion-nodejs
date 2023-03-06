1b340a49a46703c0b6f574a978b50e23a2260cac
dev <dev@ishro.com>
2023-03-06 17:09:50 +0400

    BUG FIX

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(function (req, res, next) {
 
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-access-token"
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
   );
   // Pass to next layer of middleware
 
