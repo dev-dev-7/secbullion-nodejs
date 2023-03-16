8d01a4f9c3cd32c05134f097afabd2b664901493
dev <dev@ishro.com>
2023-03-16 18:27:52 +0400

    BUG FIX

--- src/helpers/validation/appData.js
+++ src/helpers/validation/appData.js
@@ -5,4 +5,5 @@ exports.add = [check("meta_key").notEmpty(), check("meta_values").notEmpty()];
 exports.update = [
   check("meta_key").notEmpty(),
   check("meta_values").notEmpty(),
+  check("status").notEmpty(),
 ];
