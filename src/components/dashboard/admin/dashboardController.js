99938ee4b34317711e950fad8e2b6fdd1c3a7f2b
dev <dev@ishro.com>
2023-03-21 15:51:08 +0400

    BUG FIX

--- src/components/dashboard/admin/dashboardController.js
+++ src/components/dashboard/admin/dashboardController.js
@@ -5,8 +5,11 @@ exports.get = async (req, res) => {
   let result = {
     currency: process.env.DEFAULT_CURRENCY,
     today_business:100,
+    today_order:2,
     monthly_business:10000,
-    total_business:300000
+    monthly_orders:28,
+    total_business:300000,
+    total_orders:1234
   }
   return res.status(201).json({ data: result });
 };
