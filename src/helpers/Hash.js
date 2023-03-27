b4824eb5a2b278f6945f9c2f9d0b026ff37374c3
Ishro Dev <dev@ishro.com>
2023-03-27 15:26:18 +0400

    BUG FIX

--- src/helpers/Hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcryptjs");
-// TODO: use async to speed up the hashing process
-exports.make = (plain) => {
-  return bcrypt.hashSync(plain, 10);
-};
-
-exports.check = (plain, hash) => {
-  return bcrypt.compareSync(plain, hash);
-};
