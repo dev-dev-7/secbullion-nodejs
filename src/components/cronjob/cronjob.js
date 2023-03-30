6d79fa434931884de1d472a51e9722b170161b16
Ishro Dev <dev@ishro.com>
2023-03-30 11:40:47 +0400

    BUG FIX

--- src/components/cronjob/cronjob.js
+++ src/components/cronjob/cronjob.js
@@ -1,24 +1,23 @@
 require("dotenv").config();
-const {getSymbol} = require("../../helpers/mt5");
 const productModel = require("../product/productModel");
 const orderModel = require("../order/orderModel");
+const { getSymbolPrice, getPriceFromSymbol } = require("../../helpers/mt5");
 
 exports.priceUpdate = async (req, res) => {
-  let mt5PriceArray = {};
   const products = await productModel.get();
-  // if (products) {
-  //   mt5PriceArray = await getAllSymbolsPrice(products);
-  //   for (var i = 0; i < products.length; i++) {
-  //     let price = await getPriceFromSymbol(mt5PriceArray, products[i].symbol);
-  //     if (price) {
-  //       await productModel.updateProductPrice(
-  //         products[i].id,
-  //         products[i].symbol,
-  //         price
-  //       );
-  //       await orderModel.updateOrderProductPrice(products[i].id, price);
-  //     }
-  //   }
-  // }
+  if (products) {
+    let symbolPrices = await getSymbolPrice(products);
+    for (var i = 0; i < products.length; i++) {
+      let price = await getPriceFromSymbol(symbolPrices, products[i].symbol);
+      if (price) {
+        await productModel.updateProductPrice(
+          products[i].id,
+          products[i].symbol,
+          price
+        );
+        await orderModel.updateOrderProductPrice(products[i].id, price);
+      }
+    }
+  }
   return res.status(200).json({ data: products });
 };
--- src/helpers/sendEmail.js
+++ src/helpers/sendEmail.js
@@ -1,13 +1,15 @@
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const deliverEmailNotify = async (order_id, items = "") => {
+
+
+const deliverEmailNotify = async (order_id, items="") => {
   const msg = {
     to: "dev@ishro.com", // Change to your recipient
     from: "tickets@ishro.com", // Change to your verified sender
-    subject: "Secbullion Order: " + order_id,
+    subject: "Secbullion Order: "+order_id,
     text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>"
   };
   return sgMail
     .send(msg)
