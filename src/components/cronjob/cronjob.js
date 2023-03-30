bb12df407e84d60b787da132f24bfdc8c4fb9462
Ishro Dev <dev@ishro.com>
2023-03-30 13:34:30 +0400

    BUG FIX

--- src/components/cronjob/cronjob.js
+++ src/components/cronjob/cronjob.js
@@ -1,21 +1,24 @@
 require("dotenv").config();
 const productModel = require("../product/productModel");
-const orderModel = require("../order/orderModel");
+//const orderModel = require("../order/orderModel");
 const { getSymbolPrice, getPriceFromSymbol } = require("../../helpers/mt5");
 
 exports.priceUpdate = async (req, res) => {
   const products = await productModel.get();
   if (products) {
-    let symbolPrices = await getSymbolPrice(products);
-    for (var i = 0; i < products.length; i++) {
-      let price = await getPriceFromSymbol(symbolPrices, products[i].symbol);
-      if (price) {
-        await productModel.updateProductPrice(
-          products[i].id,
-          products[i].symbol,
-          price
-        );
-        await orderModel.updateOrderProductPrice(products[i].id, price);
+    for (var n = 1; n <= 7; n++) {
+      console.log("n:" + n);
+      let symbolPrices = await getSymbolPrice(products);
+      for (var i = 0; i < products.length; i++) {
+        let price = await getPriceFromSymbol(symbolPrices, products[i].symbol);
+        if (price) {
+          await productModel.updateProductPrice(
+            products[i].id,
+            products[i].symbol,
+            price
+          );
+          //await orderModel.updateOrderProductPrice(products[i].id, price);
+        }
       }
     }
   }
