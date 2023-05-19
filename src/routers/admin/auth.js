dd34f5a24ea96ce8f6e61c2e5fce1b734f5b4d3d
dev <dev@ishro.com>
2023-05-20 00:15:42 +0400

    FIXED : Access token bug

--- src/routers/admin/auth.js
+++ src/routers/admin/auth.js
@@ -4,13 +4,14 @@ const authController = require('../../components/auth/authController')
 const adminAuthController = require('../../components/auth/admin/authController')
 const profileController = require('../../components/profile/profileController')
 const validation = require('../../helpers/validation/auth')
+const verifyToken = require("../../helpers/verifyToken");
 
 // Auth
 router.post('/login', validation.login_validation, adminAuthController.login)
 
 // Users
 router.post('/users', adminAuthController.getAllUsers)
-router.put('/users/:user_id', adminAuthController.status)
+router.put('/users/:user_id',[verifyToken], adminAuthController.status)
 router.get('/user/:user_id', adminAuthController.getUser)
 router.get('/user/address/:user_id', profileController.getAllAddress)
 router.post('/user/address', profileController.getAddress)
