ed52b1afe6bd8c4ae6f4a49de0197dcdbe334e8e
Ishro Dev <dev@ishro.com>
2023-06-23 16:03:47 +0400

    FIXXXX

--- src/routers/wallet.js
+++ src/routers/wallet.js
@@ -6,7 +6,9 @@ const validation = require("../helpers/validation/checkout");
 
 // Wallet
 router.route("/wallet/:user_id").get([verifyToken], walletController.get);
-router.route("/wallet/checkout/callback").get(walletController.checkouCallback);
+router
+  .route("/wallet/checkout/callback")
+  .post(walletController.checkouCallback);
 router
   .route("/wallet/checkout/payment")
   .post([verifyToken, validation.payment], walletController.payment);
