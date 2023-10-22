d614f245fd3121f38d5c06e5a37d785895d3e599
Abdul07X00 <ihshaan10@gmail.com>
2023-10-22 22:00:51 +0400

    File upload bug fixes

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use(function (req, res, next) {
 });
 
 app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true ,  parameterLimit:50000}));
 app.use(express.static("public"));
 app.use(multerMid.any());
 
